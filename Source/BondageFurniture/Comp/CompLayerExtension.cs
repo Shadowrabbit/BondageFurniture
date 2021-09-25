949f16bb7c671316af010547200a52faf61f28a7
Shadowrabbit <yingtu0401@gmail.com>
2021-09-25 11:03:58 +0800

    移除部分日志

--- Source/BondageFurniture/Comp/CompLayerExtension.cs
+++ Source/BondageFurniture/Comp/CompLayerExtension.cs
@@ -16,18 +16,11 @@ namespace RabiSquare.BondageFurniture
     {
         private CompPropertiesLayerExtension Props => (CompPropertiesLayerExtension) props;
 
-        public override void Initialize(CompProperties props)
-        {
-            base.Initialize(props);
-        }
-
         public override void PostDraw()
         {
             base.PostDraw();
-            Log.Warning("初始化");
             if (Props.graphicLayers == null || Props.graphicLayers.Count <= 0)
             {
-                Log.Warning("没有图形层级数据");
                 return;
             }
 
@@ -35,14 +28,8 @@ namespace RabiSquare.BondageFurniture
             {
                 var location = GenThing.TrueCenter(parent.Position, parent.Rotation, parent.def.size,
                     compDataGraphicLayer.altitudeLayer.AltitudeFor());
-                if (Prefs.DevMode)
-                {
-                    Log.Message("绘制 坐标:" + location);
-                }
-
                 compDataGraphicLayer.graphicData.Graphic.Draw(location, parent.Rotation, parent);
             }
-            Log.Warning("初始化2");
         }
     }
 }
